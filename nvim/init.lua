b22509fa52d52eb108cf35b03baaa077556b9404
Scott Hudson <scott.w.hudson@gmail.com>
2024-02-03 14:39:04 -0500

    add plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,25 @@
+vim.g.mapleader = " "
+-- Keys
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "bb", "binding.b")
+
+-- window mvt
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- terminal
+vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<CR>")
+vim.keymap.set("t", "<C-j>", "<cmd>wincmd j<CR>")
+vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<CR>")
+vim.keymap.set("t", "<C-l>", "<cmd>wincmd l<CR>")
+vim.keymap.set("n", "<leader>j", ":terminal")
+-- Commands
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>t", vim.cmd.TestNearest)
+vim.keymap.set("n", "<leader>T", vim.cmd.TestFile)
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -11,13 +33,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require("lualine").setup()
-    end
-  }
-})
+require("lazy").setup("plugins")
 
