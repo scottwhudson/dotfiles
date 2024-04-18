1f893488b74cbf00ff6e0790ed6720ba5866c4f0
scotthudson <scott.hudson@tropicapp.io>
2024-04-18 16:03:36 -0400

    working formatter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -39,4 +39,11 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
-print("hello msg")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+	group = "__formatter__",
+	command = ":FormatWrite",
+})
--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -1,17 +1,25 @@
 return {
-  "mhartington/formatter.nvim",
-  config = function()
-    require("formatter").setup {
-      logging = true,
-      log_level = vim.log.levels.WARN,
-      filetype = {
-        ruby = {
-				  require("formatter.filetypes.ruby").standardrb
+	"mhartington/formatter.nvim",
+	config = function()
+		require("formatter").setup({
+			logging = true,
+			log_level = vim.log.levels.DEBUG,
+			filetype = {
+				lua = {
+					require("formatter.filetypes.lua").stylua,
+				},
+				ruby = {
+					require("formatter.filetypes.ruby").standardrb,
 				},
 				python = {
-				  require("formatter.filetypes.python").black,
-        }
-      }
-    }
-  end
+					require("formatter.filetypes.python").black,
+				},
+				["*"] = {
+					-- "formatter.filetypes.any" defines default configurations for any
+					-- filetype
+					require("formatter.filetypes.any").remove_trailing_whitespace,
+				},
+			},
+		})
+	end,
 }
